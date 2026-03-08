cd4562702add8e17225c9f05e7777a243f057171
Adrien Vannson <avannson@buf.build>
2026-03-08 10:09:23 +0100

    Run tests in just test

--- protobuf/src/root.zig
+++ protobuf/src/root.zig
@@ -3,3 +3,8 @@ const tag = @import("wire/tag.zig");
 
 pub const BinaryWriter = binary_writer.BinaryWriter;
 pub const WireType = tag.WireType;
+
+test {
+    _ = @import("wire/binary_writer.zig");
+    _ = @import("wire/tag.zig");
+}
