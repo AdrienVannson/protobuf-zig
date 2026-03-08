6b93f7fd40ceadc0b5d99808037b04ed2ce1eff2
Adrien Vannson <avannson@buf.build>
2026-03-08 09:35:23 +0100

    Remove wire.zig

--- protobuf/src/wire.zig
+++
@@ -1,4 +0,0 @@
-pub const binary_writer = @import("wire/binary_writer.zig");
-pub const tag = @import("wire/tag.zig");
-pub const BinaryWriter = binary_writer.BinaryWriter;
-pub const WireType = tag.WireType;
