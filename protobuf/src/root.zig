a58e96310d334c2aeba5c39d508d1e1f8f5ee625
Adrien <avannson@buf.build>
2026-03-09 19:45:49 +0100

    Add descriptors (#7)
    * Add descriptor types
    
    * Improve ScalarType
    
    * Remove service-related descriptors
    
    * Format code
    
    * Add numbers in FieldPresence
    
    * Fix enum type
    
    * Update enum description
    
    * Update descriptors

--- protobuf/src/root.zig
+++ protobuf/src/root.zig
@@ -1,16 +1,34 @@
 const binary_writer = @import("wire/binary_writer.zig");
 const tag = @import("wire/tag.zig");
+const descriptor = @import("descriptor.zig");
 const to_binary_mod = @import("wire/to_binary.zig");
 const from_binary_mod = @import("wire/from_binary.zig");
 
-pub const BinaryWriter = binary_writer.BinaryWriter;
 pub const WireType = tag.WireType;
+pub const ScalarType = descriptor.ScalarType;
+pub const FieldPresence = descriptor.FieldPresence;
+pub const DefaultValue = descriptor.DefaultValue;
+pub const DescMember = descriptor.DescMember;
+pub const DescElementType = descriptor.DescElementType;
+pub const DescComments = descriptor.DescComments;
+pub const DescFile = descriptor.DescFile;
+pub const DescEnum = descriptor.DescEnum;
+pub const DescEnumValue = descriptor.DescEnumValue;
+pub const DescMessage = descriptor.DescMessage;
+pub const DescOneof = descriptor.DescOneof;
+pub const DescFieldKind = descriptor.DescFieldKind;
+pub const DescField = descriptor.DescField;
+pub const DescExtensionKind = descriptor.DescExtensionKind;
+pub const DescExtension = descriptor.DescExtension;
+
+pub const BinaryWriter = binary_writer.BinaryWriter;
 pub const to_binary = to_binary_mod.to_binary;
 pub const from_binary = from_binary_mod.from_binary;
 
 test {
     _ = @import("wire/binary_writer.zig");
     _ = @import("wire/tag.zig");
+    _ = @import("descriptor.zig");
     _ = @import("wire/to_binary.zig");
     _ = @import("wire/from_binary.zig");
 }
