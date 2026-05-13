a250d4b5766d725c8dbe2078864f43861dd0b7a7
Adrien Vannson <adrien.vannson@protonmail.com>
2026-05-13 11:04:12 +0200

    Call to_binary in example

--- example/main.zig
+++ example/main.zig
@@ -2,14 +2,19 @@ const std = @import("std");
 const example = @import("gen/example.pb.zig");
 const protobuf = @import("protobuf");
 
-pub fn main() void {
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+
     const person = example.Person{
         .name = "Alice",
         .age = 30,
         .email = "alice@example.com",
     };
 
-    std.debug.print("name:  {s}\n", .{person.getName()});
-    std.debug.print("age:   {d}\n", .{person.getAge()});
-    std.debug.print("email: {s}\n", .{person.getEmail()});
+    var buf = std.ArrayList(u8){};
+    defer buf.deinit(allocator);
+    try protobuf.to_binary(allocator, person, buf.writer(allocator));
+    std.debug.print("encoded ({d} bytes): {x}\n", .{ buf.items.len, buf.items });
 }
