c0af78eb6324cfa165106c0c62513b263f951896
Adrien Vannson <adrien.vannson@protonmail.com>
2026-09-26 09:56:32 +0200

    Update comment

--- protobuf/src/json/wkt_time.zig
+++ protobuf/src/json/wkt_time.zig
@@ -120,9 +120,8 @@ pub fn formatTimestamp(buf: []u8, seconds: i64, nanos: i32) ![]const u8 {
     var frac_buf: [10]u8 = undefined;
     const frac = formatFracSeconds(&frac_buf, @intCast(nanos));
 
-    // Cast the (always non-negative, range-validated) fields to unsigned:
-    // Zig's `{d:0>N}` formatting prepends an explicit '+' for positive
-    // *signed* integers when a width is given, which we don't want here.
+    // Cast the fields to unsigned: `{d:0>N}` formatting prepends an explicit '+'
+    // for positive signed integers when a width is given, which we don't want here.
     return std.fmt.bufPrint(buf, "{d:0>4}-{d:0>2}-{d:0>2}T{d:0>2}:{d:0>2}:{d:0>2}{s}Z", .{
         @as(u32, @intCast(ymd.year)),
         @as(u32, @intCast(ymd.month)),
